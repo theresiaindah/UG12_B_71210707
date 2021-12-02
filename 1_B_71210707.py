60d9eda423be5291ab99a1e97949867720b8f6a6
theresiaindah <93325924+theresiaindah@users.noreply.github.com>
2021-12-03 00:25:28 +0700

    soal 1

---
+++ 1_B_71210707.py
@@ -0,0 +1,54 @@
+#soal 1 task 1
+n = int(input("Masukkan banyak bilangan: "))
+print("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0")
+
+# list number
+numbers = [ 1 + 2 - 3 + 4 + 5 - 6 + 0]
+
+# variabel untuk menyimpan jumlah 
+sum = 0 
+
+# iterasi 
+for each in numbers:
+    sum = sum + each
+
+# output : jumlah semuanya 
+print("Hasil perhitungan diatas ialah: ", sum)
+
+
+#soal 1 task 2
+n = int(input("Masukkan banyak bilangan: "))
+print("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12 ")
+
+# list number
+numbers = [ 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12]
+
+# variabel untuk menyimpan jumlah 
+sum = 0 
+
+# iterasi 
+for each in numbers:
+    sum = sum + each
+
+# output : jumlah semuanya 
+print("Hasil perhitungan diatas ialah: ", sum)
+
+
+#soal 1 task 3
+
+#soal 1 task 2
+n = int(input("Masukkan banyak bilangan: "))
+print("Total = 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12 + 13 + 0 ")
+
+# list number
+numbers = [ 1 + 2 - 3 + 4 + 5 - 6 + 0 + 8 - 9 + 10 + 11 - 12 + 13 + 0]
+
+# variabel untuk menyimpan jumlah 
+sum = 0 
+
+# iterasi 
+for each in numbers:
+    sum = sum + each
+
+# output : jumlah semuanya 
+print("Hasil perhitungan diatas ialah: ", sum)
